40318861d2dd1b5c1337fe5432d169eaf3ce2fab
LilianLima <lilian_santoslima@hotmail.com>
2019-01-30 09:03:27 -0200

    Commit inicial

---
+++ src/test/java/com/configservice/base/AppConfigServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.configservice.base;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AppConfigServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
